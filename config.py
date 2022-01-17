1898d1e0b91242a24a32d320a65363c04aa292cd
vetallb <garuk1vitalik@gmail.com>
2022-01-17 15:58:02 +0200

    posting bot

---
+++ config.py
@@ -0,0 +1,19 @@
+import imp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+REDDIT_ID = os.getenv('REDDIT_ID')
+REDDIT_SECRET = os.getenv('REDDIT_SECRET')
+
+
+
+
+
+
+
+
---
+++ loader.py
@@ -0,0 +1,65 @@
+from tkinter import PhotoImage
+from xmlrpc import client
+from config import BOT_TOKEN,CHANNEL_ID,REDDIT_ID,REDDIT_SECRET
+from aiogram import Bot, types
+import asyncpraw
+import asyncio
+
+
+
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+
+
+reddit = asyncpraw.Reddit(
+    client_id=REDDIT_ID,
+    client_secret=REDDIT_SECRET,
+    user_agent="random_reddit_bot/0.0.1",
+)
+
+print(reddit)
+
+
+
+posted = []
+TIMEOUT = 360
+SUBREDDIT_NAME = 'wholesomeanimemes'
+POST_LIMIT = 1
+
+
+async def send_message(channel_id: int, text: str, post_url: str):
+    await bot.send_photo(channel_id,post_url, text)
+
+
+
+async def main():
+    while True:
+        await asyncio.sleep(TIMEOUT)
+        memes_submission = await reddit.subreddit(SUBREDDIT_NAME)
+        memes_submission = memes_submission.new(limit=POST_LIMIT)
+        item = await memes_submission.__anext__()
+        if item.title not in posted:
+            posted.append(item.title)
+            try:
+                await send_message(CHANNEL_ID,item.title, item.url)
+            except Exception as err:
+                bot.send_message(682382931, err)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
